158e90f1fd3b9b2a5b442176b4943341728efe7a
Nils Wiersma <nils.wiersma@gmail.com>
2020-08-16 09:21:04 +0200

    added snapshot and multiprocess example

--- QilingFi.py
+++ QilingFi.py
@@ -16,17 +16,39 @@ def disasm(ql, addr, size, buf=None):
 	for address,size,mnemonic,op_str in ql.md.disasm_lite(buf, addr):
 		t += ql.trace_fstring.format(address, buf[::-1].hex(), mnemonic, op_str)
 		if ql.console:
-			print(t)
+			print(f'[{ql.ins_counter:05d}] ' + t)
 	return t	
+
+def ins_counter(ql, *args):
+	ql.ins_counter += 1
 
 def asm_trace(ql, address, size):
 	if address == ql.trace_start:
 		ql.tracing = True
+		if ql.console:
+			print(f'[!] start tracing at {hex(address)}')
 	if address == ql.trace_end:
 		ql.tracing = False
+		if ql.console:
+			print(f'[!] end tracing at {hex(address)}')
 
 	if ql.tracing:
-		ql.trace.append(disasm(ql, address, size))	
+		ql.trace.append(disasm(ql, address, size))
+
+def make_snapshot(ql, address, size):
+	if address == ql.snapshot_addr:
+		if ql.console:
+			print(f'[!] creating snapshot at {hex(address)} in', end=' ')
+
+		if ql.snapshot_file:
+			ql.save(reg=False, cpu_context=True, snapshot=ql.snapshot_file)
+			if ql.console:
+				print(f'{ql.snapshot_file}')
+		else:
+			ql.snapshot = ql.save(reg=False, cpu_context=True)
+			if ql.console:
+				print('ql.snapshot')	
+		ql.emu_stop()
 
 class QilingFi(Qiling):
 	"""Fault injection helpers for Qiling run"""
@@ -37,8 +59,10 @@ class QilingFi(Qiling):
 
 		self.trace_start = kwargs.pop('trace_start')
 		self.trace_end = kwargs.pop('trace_end')
-		self.fi_args = kwargs.pop('fi_args')
-		self.fi_model = kwargs.pop('fi_model')
+		self.snapshot_addr = kwargs.pop('snapshot_addr', None)
+		self.snapshot_file = kwargs.pop('snapshot_file', None)
+		self.fi_args = kwargs.pop('fi_args', None)
+		self.fi_model = kwargs.pop('fi_model', None)
 		self.md = kwargs.pop('md')
 
 		super(QilingFi, self).__init__(*args, **kwargs)
@@ -46,9 +70,16 @@ class QilingFi(Qiling):
 		self.tracing = False
 		self.trace = []
 		self.trace_fstring = '{:x}:  {}  {} {}'
+		self.ins_counter = 0
 
 		self.hook_code(asm_trace)
+		self.hook_code(ins_counter)
 
+		if self.snapshot_addr:
+			self.hook_code(make_snapshot)
+
+
+	"""Extending some existing Qiling functions"""
 	def run(self, *args, **kwargs):
 		if self.fi_model:
 			self.fi_model(self)
---
+++ ifelse_snapshot_compare.py
@@ -0,0 +1,123 @@
+# Generic imports
+import sys, os, time, random
+import sqlite3
+import signal
+
+from collections import namedtuple
+TqdmIt = namedtuple('TqdmIt', ['tqdm', 'it'])
+
+# Installed together with qiling
+import unicorn
+import capstone
+
+# Other installed packages
+from tqdm.contrib.itertools import product as tproduct
+from tqdm import tqdm, trange
+from tqdm.auto import tqdm as tqdm_auto
+from tqdm.contrib.concurrent import process_map
+
+# Custom imports
+from QilingFi import QilingFi
+from helpers import StringBuffer, MyDB
+from helpers import tproduct, tproduct_builder
+from fault_models import flip_single_bit, random_word, set_word
+
+console = False
+terminal_logging = False
+
+MAIN_START = 0x102ec
+TRACE_END   = 0x10308
+
+def pool_f(*args):
+    exception = ''
+    ql2 = QilingFi(
+        # Qiling args
+        ["ifelse/ifelse"], ".", 
+        console=console, stdin=StringBuffer(), stdout=StringBuffer(),
+
+        # QilingFi args
+        trace_start=MAIN_START, trace_end=TRACE_END,
+        fi_model=random_word, fi_args=fi_args,
+        md=md
+        )
+
+    try:
+        if sys.argv[1] == 'simple':
+            # provide a timeout as some faults will trigger some deadlock, not sure how the scale works here
+            ql2.run(timeout=1000000)
+        else:
+            ql2.restore(ql.snapshot)
+            # continue tracing (or not) depending on snapshot
+            ql2.tracing = ql.tracing
+            # restore instruction counter state
+            ql2.ins_counter = ql.ins_counter
+            # create snapshot after addr-4 execution, continue from addr
+            ql2.run(begin=addr, timeout=1000000)
+    except Exception as e:
+        # print('exception')
+        exception = str(e)
+        ql2.emu_stop()
+
+    # read out data from the emulation
+    data = ql2.stdout.read_all()
+
+    if sys.argv[1] == 'snapshotmulti':
+        return (ql2.fi_args['ins'], data, exception)
+    else:
+        tqdm.write(str((data, exception)))
+
+def pause_or_quit(signum, frame):
+    # https://stackoverflow.com/questions/18114560/python-catch-ctrl-c-command-prompt-really-want-to-quit-y-n-resume-executi
+    # restore the original signal handler as otherwise evil things will happen
+    # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
+    signal.signal(signal.SIGINT, original_sigint)
+
+    try:
+        input('Script paused, hit CTRL-C again to quit, Enter to continue > ')
+    except KeyboardInterrupt:
+        print('Killing script')
+        sys.exit(1)
+
+    # restore the exit gracefully handler here    
+    signal.signal(signal.SIGINT, pause_or_quit)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or sys.argv[1] not in ['simple', 'snapshot', 'snapshotmulti']: 
+        print(f'Usage: python3 {sys.argv[0]} [simple|snapshot|snapshotmulti]')
+        sys.exit(1)
+
+    ARCH = capstone.CS_ARCH_ARM
+    MODE = capstone.CS_MODE_ARM
+
+    NRUNS = 1000
+
+    md = capstone.Cs(ARCH, MODE)
+
+    addr = 0x102f8#4#ec#0x102f8
+
+    # arguments for fi_model
+    fi_args = {
+        'addr': addr,
+        'ins': b'\x00'*4
+    }
+
+    ql = QilingFi(
+        # Qiling args
+        ["ifelse/ifelse"], ".", 
+        console=console, stdin=StringBuffer(), stdout=StringBuffer(),
+
+        # QilingFi args
+        trace_start=MAIN_START, trace_end=TRACE_END,
+        snapshot_addr=addr-4, #snapshot_file='.snapshot.bin',
+        md=md
+        )
+
+    ql.run()
+
+    if sys.argv[1] == 'snapshotmulti':
+        ret = process_map(pool_f, range(NRUNS))
+        for x in ret:
+            print(x)
+    else:
+        for _ in trange(NRUNS):
+            pool_f()
